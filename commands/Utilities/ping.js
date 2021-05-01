f649807dc83152e5d3fd738778682e8910b51822
APartOfMe <jordanwyman1@gmail.com>
2021-05-01 12:31:04 -0500

    Fixed variable issue :blobsweat:

--- commands/Utilities/ping.js
+++ commands/Utilities/ping.js
@@ -5,6 +5,6 @@ module.exports = {
 	 async execute(msg, args) {
 		var ping = await msg.channel.send("Fetching ping...");
 		
-		ping.edit(`Pong! Heartbeat is ${client.ws.ping}ms,\nMessage Roundtrip took ${mess.createdTimestamp - msg.createdTimestamp}ms.`);
+		ping.edit(`Pong! Heartbeat is ${client.ws.ping}ms,\nMessage Roundtrip took ${ping.createdTimestamp - msg.createdTimestamp}ms.`);
 	},
 };
