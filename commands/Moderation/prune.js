c7a0193900ed6d63c5566ec06df50c1b479c4ad7
APartOfMe <jordanwyman1@gmail.com>
2021-06-02 20:15:20 -0500

    Better error handling

--- commands/Moderation/prune.js
+++ commands/Moderation/prune.js
@@ -6,7 +6,6 @@ module.exports = {
   usage: '`{prefix}prune <number>`',
   examples: '`{prefix}prune 15`',
   async execute(msg, args) {
-
     if (!msg.member.hasPermission('MANAGE_MESSAGES')) { //If the author can't delete messages, send an error
       return msg.reply("Sorry! You can't prune messages");
     };
@@ -15,17 +14,38 @@ module.exports = {
       return msg.author.send(`I can't delete messages! I need the \`Manage Messages\` permission`);
     };
 
-    const deleteCount = parseInt(args[0], 10); //Get the number of messages to delete
+    const deleteCount = parseInt(args[0]); //Get the number of messages to delete
 
     if (!deleteCount || deleteCount < 2 || deleteCount > 100) { //Check if the number of messages to delete is greater than 1, and less than 100
       return msg.reply("Please provide a number between 2 and 100 for the number of messages to delete");
     };
 
-    const fetched = await msg.channel.messages.fetch({ //Get the specified number of messages
-      limit: deleteCount
-    });
+    var tries = 0;
 
-    msg.channel.bulkDelete(fetched)
-      .catch(error => msg.reply(`Couldn't delete messages because of: ${error}`));
+    const fetchMsgs = async (int) => {
+      try {
+        tries++;
+
+        const fetched = await msg.channel.messages.fetch({ //Get the specified number of messages
+          limit: int
+        });
+
+        return fetched;
+      } catch (error) {
+        if (tries < 3) { //Retry a few times if there was an error
+          return fetchMsgs(int);
+        } else {
+          return false;
+        };
+      };
+    };
+
+    const toDelete = await fetchMsgs(deleteCount);
+
+    if (toDelete) {
+      msg.channel.bulkDelete(toDelete).catch(error => msg.reply(`Couldn't delete messages because of: ${error}`));
+    } else {
+      return msg.channel.send("There was an error fetching messages. Please try again later");
+    };
   },
 };
